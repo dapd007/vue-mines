8626d334461d8f64e1b8b82554adf7c7a2975326
David Paternina <david@fanxielab.com>
2018-04-18 00:54:39 -0500

    Recursion fix

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,10 +3,24 @@
     <div class="hero-body">
       <div class="container">
         <div class="columns is-centered">
-          <div class="column is-8">
-            <div class="columns game-row" v-for="row in rows">
+          <div class="column is-10">
+            <div class="columns game-row" v-for="row in rows" @click.right.prevent="() => {}">
               <div class="column game-col" :key="cell.row + '-' + cell.column" v-for="cell in row">
-                <game-cell :cell="cell"></game-cell>
+                <div class="ms-box"
+                     @dblclick="revealAdjacent(cell)"
+                     @click.left="revealCell(cell)"
+                     @click.right.prevent="flagCell(cell)"
+                     :class="{
+                       'is-blank': cell.mineCount === 0,
+                       'is-flag' : cell.isFlagged,
+                       'is-mine': cell.isMine,
+                       'is-revealed': cell.isRevealed
+                     }"
+                >
+                  <div class="cell-content" v-if="cell.isRevealed">
+                    <span class="cell-number" v-if="cell.mineCount > 0">{{ cell.mineCount }}</span>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -17,23 +31,20 @@
 </template>
 
 <script>
-  import GameCell from './GameCell';
   import { EventBus } from "../EventBus";
 
   export default {
     name: "game",
-    components: { GameCell },
     data() {
       return {
         mines: [],
         rows: [],
-        rowsNumber: 8,
-        columnsNumber: 8,
-        minesNumber: 10
+        rowsNumber: 16,
+        columnsNumber: 30,
+        minesNumber: 99
       }
     },
     mounted() {
-      EventBus.$on('reveal-adjacent', this.revealAdjacent);
       //  Create basic board
       this.createBoard();
       //  Set random mines
@@ -52,9 +63,12 @@
           for ( let j = 0; j < this.columnsNumber; j++ ) {
 
             const cell = {
-              value: 0,
+              mineCount: 0,
               row: i,
               column: j,
+              isMine: false,
+              isFlagged: false,
+              isRevealed: false
               // isMine: mines.filter( mine => mine.row === i && mine.col === j).length > 0
             };
 
@@ -96,26 +110,47 @@
           const adjacentCells = this.getAdjacentCells(mine);
           adjacentCells.forEach( cell => {
             if ( !cell.isMine ) {
-              cell.value++;
+              cell.mineCount++;
             }
           });
         });
       },
-      revealAdjacent(mine) {
-        const adjacent = this.getAdjacentCells(mine);
-
-        adjacent.forEach( cell => {
-          if ( !cell.isMine ) {
-
-            EventBus.$emit('reveal', cell);
-
-            // if ( cell.value === 0 ) {
-            //   // this.revealAdjacent(cell);
-            // } else {
-            //
-            // }
+      //  Game actions
+      revealCell(cell) {
+        if ( !cell.isRevealed ) {
+          cell.isRevealed = true;
+          if ( cell.isMine && !cell.isFlagged ) {
+            console.log('gg');
+          } else {
+            this.revealAdjacent(cell);
           }
-        });
+        }
+      },
+      revealAdjacent(cell) {
+        if ( cell.isRevealed ) {
+          const adjacent = this.getAdjacentCells(cell);
+          adjacent.forEach( adj => {
+            if ( !adj.isMine && !adj.isRevealed ) {
+
+              adj.isRevealed = true;
+
+              if ( adj.mineCount === 0 ) {
+                this.revealAdjacent(adj);
+              }
+            }
+          });
+        }
+      },
+      handleDoubleClick() {
+        //  todo
+        //  if adjacentFlags === adjacentMines
+        //    if adjacentFlags are indeed mines, revealAdjacent
+        //    else GG
+      },
+      flagCell(cell) {
+        if ( !cell.isRevealed ) {
+          cell.isFlagged = !cell.isFlagged;
+        }
       }
     }
   }
@@ -124,5 +159,53 @@
 <style lang="scss" scoped>
   .game-col {
     padding: 0.2rem;
+  }
+
+  .ms-box {
+    cursor: pointer;
+    border-radius: 3px;
+    position: relative;
+    background-color:hsl(0, 0%, 75%);
+
+    -webkit-user-select: none; /* Safari 3.1+ */
+    -moz-user-select: none; /* Firefox 2+ */
+    -ms-user-select: none; /* IE 10+ */
+    user-select: none; /* Standard syntax */
+
+    &.is-revealed {
+      background-color:hsl(0, 0%, 90%);
+    }
+
+    &.is-revealed.is-blank {
+      opacity: .6;
+    }
+
+    &.is-mine.is-revealed {
+      background-color: hsl(348, 100%, 61%);
+    }
+
+    &.is-flag {
+      background-color: hsl(204, 86%, 53%);
+    }
+
+    &:after {
+      content: '';
+      display: block;
+      padding-bottom: 100%;
+    }
+
+    .cell-content {
+      position: absolute;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 26px;
+    }
   }
 </style>
